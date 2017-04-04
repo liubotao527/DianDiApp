a5d5ba42f7586647a68e8781e7b086613f66d486
caohao <705083979@qq.com>
2017-04-04 15:47:25 +0800

    编辑页面

--- app/src/main/com/example/xdcao/diandiapp/UI/songwenqiang/ui/SignUpActivity.java
+++ app/src/main/com/example/xdcao/diandiapp/UI/songwenqiang/ui/SignUpActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.xdcao.diandiapp.R;
@@ -14,12 +16,22 @@ import com.example.xdcao.diandiapp.UI.songwenqiang.prefs.UserPrefs;
 public class SignUpActivity extends AppCompatActivity {
     public UserPrefs userPrefs;
     public TextView skipTextView;
+    private EditText telephone;
+    private EditText password;
+    private Button login;
+    private Button signUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
         skipTextView = (TextView) findViewById(R.id.skip);
+
+        telephone=(EditText)findViewById(R.id.email);
+        password=(EditText)findViewById(R.id.password);
+
+
+
         userPrefs = new UserPrefs(SignUpActivity.this);
         if(!TextUtils.isEmpty(userPrefs.getAuthToken())){
             startActivity(new Intent(SignUpActivity.this,MainActivity.class));

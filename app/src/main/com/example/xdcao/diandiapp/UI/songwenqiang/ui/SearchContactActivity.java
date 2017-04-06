cc5c6d2cda92cc724c5ece7986db22309fa88c59
wewarrios <1214597188@QQ.com>
2017-04-06 17:11:48 +0800

    添加联系人查询

--- app/src/main/com/example/xdcao/diandiapp/UI/songwenqiang/ui/SearchContactActivity.java
+++ app/src/main/com/example/xdcao/diandiapp/UI/songwenqiang/ui/SearchContactActivity.java
@@ -3,9 +3,11 @@ package com.example.xdcao.diandiapp.UI.songwenqiang.ui;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.SearchView;
 import android.widget.TextView;
@@ -32,15 +34,22 @@ public class SearchContactActivity extends AppCompatActivity {
     private RoundImageView mRivPhoto;
     private ImageView mIvAdd;
 
-    private MyUser returnedUser=null;
+    private CardView mCvAddContact;
+
+
 
     android.os.Handler handler=new android.os.Handler() {
         @Override
         public void handleMessage(Message msg) {
             switch (msg.what){
                 case HandlerCons.QUERY_GIVEN_USER:
-                    Log.d(TAG, "handleMessage: "+"get handler user name: "+returnedUser.getUsername());
-                    // TODO: 2017/4/6 留给你去展现 数据从returnedUser中获取
+                    MyUser myUser = (MyUser) msg.getData().getSerializable("MyUser");
+                    Log.d(TAG, "handleMessage: "+"get handler user name: "+myUser.getUsername());
+
+                    mTvName.setText(myUser.getUsername());
+                    //TODO 将个性签名添加到mTvSign   将个人头像添加到 mIvAdd
+                    mCvAddContact.setVisibility(View.VISIBLE);
+
 
 
 
@@ -62,8 +71,14 @@ public class SearchContactActivity extends AppCompatActivity {
             public boolean onQueryTextSubmit(String query) {
                 //添加查询的代码
                 //在一个线程中查询
-                Toast.makeText(SearchContactActivity.this,"2222",Toast.LENGTH_LONG).show();
-                queryForGivenName(query);
+                final String queryName = query;
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        queryForGivenName(queryName);
+                    }
+                }).start();
+
                 return false;
             }
 
@@ -82,6 +97,13 @@ public class SearchContactActivity extends AppCompatActivity {
         mTvSign = (TextView) findViewById(R.id.tv_sign);
         mIvAdd = (ImageView) findViewById(R.id.iv_add);
         mRivPhoto = (RoundImageView) findViewById(R.id.riv_photo);
+        mCvAddContact = (CardView) findViewById(R.id.contact_add_item);
+        mIvAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO 将此联系人添加到联系人列表
+            }
+        });
     }
 
 
@@ -96,15 +118,20 @@ public class SearchContactActivity extends AppCompatActivity {
             @Override
             public void done(List<MyUser> list, BmobException e) {
                 boolean flag=false;
+                MyUser returnedUser=null;
                 if(e==null){
                     if (list.size()>0){
+                        Log.d(TAG, "done: lllll");
                         returnedUser=list.get(0);
                         flag=true;
                     }
                 }
                 if (flag){
-                    Message message=new Message();
+                    Message message=handler.obtainMessage();
                     message.what=HandlerCons.QUERY_GIVEN_USER;
+                    Bundle b = new Bundle();
+                    b.putSerializable("MyUser",returnedUser);
+                    message.setData(b);
                     handler.sendMessage(message);
                 }
             }
